edda24e2c5ff2031071d896f88649c80ed8244eb
Sandro Janotte <sjanotte@gmail.com>
2020-10-29 23:52:27 -0300

    Adicionado o componente course info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
-import { ReplacePipe } from './pipe/replace.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { StarComponent } from './start/start.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { CourseInfoComponent } from './courses/course-info.component';
+import { Error404Component } from './error-404/error-404.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from './course.model';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
