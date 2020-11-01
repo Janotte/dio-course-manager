1b900c2ba40390ba22a336c74faa4323d04afbc6
Sandro Janotte <sjanotte@gmail.com>
2020-11-01 09:55:55 -0300

    Resolvido conflitos de merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { StarComponent } from './shared/components/star/star.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
 import { CoursesModule } from './courses/courses.module';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StarComponent
   ],
   imports: [
     BrowserModule,
